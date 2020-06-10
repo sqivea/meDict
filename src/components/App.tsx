d9cf16d714542483795b46aa6d25075507f6c52f
mavedev <usatchoff@gmail.com>
2020-06-11 00:46:16 +0300

    The App structure provided with Grid CSS

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import cn from 'styles/App.module.scss';
 
 function App() {
   return (
-    <div className='App'>
-      <p>^_^</p>
+    <div className={cn['App']}>
+      <header className={cn['App__TopPanel']}>Top panel</header>
+      <main className={cn['App__Content']}>Content</main>
+      <aside className={cn['App__CalendarWrapper']}>Calendar wrapper</aside>
     </div>
   );
 }
