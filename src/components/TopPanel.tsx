24489013cba1f32feae7166924fbef494526d09b
mavedev <usatchoff@gmail.com>
2020-06-11 01:19:54 +0300

    The TopPanel component provided

---
+++ src/components/TopPanel.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TopPanel = () => (
+  <div>
+    <h3>
+      <span><img src='assets/images/logo.png' alt='' /></span>
+      meDict
+    </h3>
+  </div>
+);
+
+export default TopPanel;
