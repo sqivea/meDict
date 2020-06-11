b554eeffe355a4fb1f8e543732884c0eddf56cb7
mavedev <usatchoff@gmail.com>
2020-06-11 16:43:17 +0300

    The Content styles provided

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import cn from 'styles/Content.module.scss';
 
 const Content = () => (
-  <div className='MainWrapper' />
+  <div className={cn['MainWrapper']}>
+    <p>Content</p>
+  </div>
 );
 
 export default Content;
