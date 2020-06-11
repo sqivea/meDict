53ef029f3b7516e3198dde3132effe1bd4466fa7
mavedev <usatchoff@gmail.com>
2020-06-11 15:49:12 +0300

    The TopPanel styles updated

--- src/components/TopPanel.tsx
+++ src/components/TopPanel.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import cn from 'styles/TopPanel.module.scss';
 
 const TopPanel = () => (
-  <div>
-    <h3>
-      <img src='logo.png' alt='logo' />
-      meDict
-    </h3>
+  <div className={cn['MainContainer']}>
+    <img
+      className={cn['MainContainer__Logo']}
+      src='logo.png'
+      alt='logo'
+    />
+    <h3 className={cn['MainContainer__Title']}>meDict</h3>
   </div>
 );
 
