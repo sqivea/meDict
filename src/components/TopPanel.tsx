bfeace559df086579b06806c3ea7e03a181d453a
mavedev <usatchoff@gmail.com>
2020-06-11 16:55:21 +0300

    The TopPanel's title style updated

--- src/components/TopPanel.tsx
+++ src/components/TopPanel.tsx
@@ -4,12 +4,14 @@ import cn from 'styles/TopPanel.module.scss';
 const TopPanel = () => (
   <div className={cn['MainContainer']}>
     <h3 className={cn['MainContainer__Title']}>m</h3>
-    <img
-      className={cn['MainContainer__Logo']}
-      src='logo.png'
-      alt='logo'
-    />
-    <h3 className={cn['MainContainer__Title']}>Dict</h3>
+    <div className={cn['MainConteainer__LogoWrapper']}>
+      <img
+        className={cn['MainContainer__Logo']}
+        src='logo.png'
+        alt='logo'
+      />
+    </div>
+    <h3 className={cn['MainContainer__Title']}>dict</h3>
   </div>
 );
 
