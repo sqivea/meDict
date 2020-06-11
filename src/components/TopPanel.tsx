3793446c489aae09abcb7d0c158e831fb48a4ea1
mavedev <usatchoff@gmail.com>
2020-06-11 16:09:29 +0300

    Custom fonts added

--- src/components/TopPanel.tsx
+++ src/components/TopPanel.tsx
@@ -3,12 +3,13 @@ import cn from 'styles/TopPanel.module.scss';
 
 const TopPanel = () => (
   <div className={cn['MainContainer']}>
+    <h3 className={cn['MainContainer__Title']}>m</h3>
     <img
       className={cn['MainContainer__Logo']}
       src='logo.png'
       alt='logo'
     />
-    <h3 className={cn['MainContainer__Title']}>meDict</h3>
+    <h3 className={cn['MainContainer__Title']}>Dict</h3>
   </div>
 );
 
