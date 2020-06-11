ed5c058cbca77e13dc88deaa7b812a985be4c373
mavedev <usatchoff@gmail.com>
2020-06-11 21:41:23 +0300

    The TitleBar component structure updated

--- src/components/TitleBar.tsx
+++ src/components/TitleBar.tsx
@@ -3,22 +3,30 @@ import cn from 'styles/TitleBar.module.scss';
 
 const app = window.require('electron').remote;
 
-const minimize = () => {
-  app.getCurrentWindow().minimize();
-};
-
-const close = () => {
-  app.getCurrentWindow().close();
-};
-
 const TitleBar = () => (
   <div className={cn['TitleBar']}>
     <nav className={cn['TitleBar__Nav']}>
       <div className={cn['TitleBar__Buttons']}>
-        <div onClick={minimize} className={cn['TitleBar__Minimize']}>
+        <div
+          /* To avoid '(...) require a role' warning. */
+          role='button'
+          /* To avoid '(...) must be focusable' warning. */
+          tabIndex={0}
+          onClick={() => app.getCurrentWindow().minimize()}
+          onKeyDown={() => app.getCurrentWindow().minimize()}
+          className={cn['TitleBar__Minimize']}
+        >
           <span>-</span>
         </div>
-        <div onClick={close} className={cn['TitleBar__Close']}>
+        <div
+          /* To avoid '(...) require a role' warning. */
+          role='button'
+          /* To avoid '(...) must be focusable' warning. */
+          tabIndex={0}
+          onClick={() => app.getCurrentWindow().close()}
+          onKeyDown={() => app.getCurrentWindow().close()}
+          className={cn['TitleBar__Close']}
+        >
           <span>&times;</span>
         </div>
       </div>
