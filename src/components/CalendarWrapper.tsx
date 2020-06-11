00f8e8d41485356ce0d465a1ec811c6ec570de50
mavedev <usatchoff@gmail.com>
2020-06-11 18:07:52 +0300

    ReactCalendar styles override provided

--- src/components/CalendarWrapper.tsx
+++ src/components/CalendarWrapper.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Calendar from 'react-calendar';
 import cn from 'styles/CalendarWrapper.module.scss';
+
+import 'react-calendar/dist/Calendar.css';
+import 'styles/overrides/Calendar.scss';
 
 const CalendarWrapper = () => (
   <div className={cn['MainWrapper']}>
-    <p>Calendar</p>
+    <Calendar className={cn['Calendar']} />
   </div>
 );
 
