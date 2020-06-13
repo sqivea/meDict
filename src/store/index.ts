b8902b5d3bbedb7cb44721a79b866fbd9f7893b2
mavedev <usatchoff@gmail.com>
2020-06-14 00:15:56 +0300

    The root state reducer provided

---
+++ src/store/index.ts
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+import { calendarReducer } from './calendar/reducers';
+
+const rootReducer = combineReducers({
+  // Allows to combine any new reducers.
+  calendar: calendarReducer
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
