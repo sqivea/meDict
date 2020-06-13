ec27a2dd6ada2844e1c6d1b468ada677e602e561
mavedev <usatchoff@gmail.com>
2020-06-13 20:58:30 +0300

    The read query implemented

--- src/db/DAO.ts
+++ src/db/DAO.ts
@@ -1,7 +1,14 @@
 import { existsSync } from 'fs';
 import sqlite3 from 'sqlite3';
 
+import { Word } from './DBObjects';
+
 const defaultDBPath = './words.db';
+
+interface ReadQueryResult {
+  word: string;
+  date: string;
+}
 
 class DAO {
   private connection: sqlite3.Database | null = null;
@@ -10,6 +17,35 @@ class DAO {
     if (!DAO.checkIfExists(dbPath)) {
       this.createDB(dbPath);
     }
+  }
+
+  public create(word: Word): void {
+    /* eslint no-unused-expressions: off */
+    this.connection?.run(
+      `INSERT INTO words VALUES(
+        '${word.value}',
+        '${word.date}'
+      )`
+    );
+  }
+
+  public read(date: Date): Word[] {
+    let result: Word[] = [];
+    /* eslint no-unused-expressions: off */
+    this.connection?.all(
+      `SELECT *
+       FROM words
+       WHERE words.date = ${date.toUTCString()}
+       ORDER BY words.word`,
+      (error, rows) => {
+        if (error) {
+          throw error;
+        } else {
+          result = DAO.getWordsFromQueryResult(rows);
+        }
+      }
+    );
+    return result;
   }
 
   private createDB(dbPath: string): void {
@@ -27,5 +63,13 @@ class DAO {
 
   private static checkIfExists(dbPath: string): boolean {
     return existsSync(dbPath);
+  }
+
+  private static getWordsFromQueryResult(rows: ReadQueryResult[]): Word[] {
+    const result: Word[] = [];
+    rows.forEach((value) => {
+      result.push(new Word(value.word, value.date));
+    });
+    return result;
   }
 }
--- src/db/DBObjects.ts
+++ src/db/DBObjects.ts
@@ -1,7 +1,7 @@
-export default class Word {
-  constructor(public word: string, public date: string) {}
+export class Word {
+  constructor(public value: string, public date: string) {}
 
   public equals(that: Word): boolean {
-    return this.word === that.word && this.date === that.date;
+    return this.value === that.value && this.date === that.date;
   }
 }
