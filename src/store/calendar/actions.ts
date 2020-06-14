937131405fecebaafcd02afd6d3ca50b1340a842
mavedev <usatchoff@gmail.com>
2020-06-14 17:09:07 +0300

    Store mistakes fixed

--- src/store/calendar/actions.ts
+++ src/store/calendar/actions.ts
@@ -1,10 +1,10 @@
 import {
   DateState,
   CALENDAR_CHANGE_DATE,
-  CalendarActionTypes
+  CalendarActionType
 } from './types';
 
-export function changeDate(newDate: DateState): CalendarActionTypes {
+export function changeDate(newDate: Date): CalendarActionType {
   return {
     type: CALENDAR_CHANGE_DATE,
     payload: newDate
