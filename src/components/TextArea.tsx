3c464312649106faf98f4421f5df55d98cfdb555
mavedev <usatchoff@gmail.com>
2020-06-15 19:33:45 +0300

    The TextArea component provided

---
+++ src/components/TextArea.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TextArea = () => (
+  <div className='MainWrapper'>
+    <textarea />
+  </div>
+);
+
+export default TextArea;
--- src/components/WordEntry.tsx
+++ src/components/WordEntry.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const WordEntry = () => (
-  <div className='MainWrapper' />
+  <div className='MainWrapper'>
+    <div className='WordForm'>
+      <textarea />
+    </div>
+  </div>
 );
 
 export default WordEntry;
