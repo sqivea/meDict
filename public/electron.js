7c13d7ce07b2a5c715d3977a45c714aaed57d259
mavedev <usatchoff@gmail.com>
2020-06-15 21:21:59 +0300

    Buttons border styles updated

--- public/electron.js
+++ public/electron.js
@@ -29,6 +29,7 @@ function createWindow() {
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
   mainWindow.on('closed', () => mainWindow = null);
   mainWindow.setMenu(null);
+  mainWindow.webContents.openDevTools();
 }
 
 (() => {
