6ed9f5a35e2cd1d2a08bfd8c2f301e9136fc1396
mavedev <usatchoff@gmail.com>
2020-06-15 19:53:54 +0300

    TextArea's props moved to the WordEntry component

--- src/components/WordEntry.tsx
+++ src/components/WordEntry.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import PropTypes, { InferProps } from 'prop-types';
 
-const WordEntry = () => (
-  <div className='MainWrapper'>
-    <div className='WordForm'>
-      <textarea />
+function WordEntry({
+  word,
+  comment
+}: InferProps<typeof WordEntry.propTypes>) {
+  return (
+    <div className='MainWrapper'>
+      <div className='WordForm'>
+        <textarea>{word}</textarea>
+        <textarea>{comment}</textarea>
+      </div>
     </div>
-  </div>
-);
+  );
+}
+
+WordEntry.propTypes = {
+  word: PropTypes.string.isRequired,
+  comment: PropTypes.string.isRequired
+};
 
 export default WordEntry;
