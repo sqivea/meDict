a3510a88b05febdbcff9da6d58f911b71de357d1
mavedev <usatchoff@gmail.com>
2020-06-16 19:03:30 +0300

    The AdderPanel structure and styles updated

--- src/components/AdderPanel.tsx
+++ src/components/AdderPanel.tsx
@@ -5,8 +5,20 @@ import cn from 'styles/AdderPanel.module.scss';
 const AdderPanel = () => (
   <div className={cn['MainWrapper']}>
     <div className={cn['AdderForm']}>
-      <textarea />
-      <textarea />
+      <div className={cn['TextAreaWrapper']}>
+        <textarea className={cn['AdderForm__TextArea']} />
+      </div>
+      <div className={cn['TextAreaWrapper']}>
+        <textarea className={cn['AdderForm__TextArea']} />
+      </div>
+      <div className={cn['ButtonWrapper']}>
+        <button
+          className={cn['AdderForm__Button']}
+          type='button'
+        >
+          Add
+        </button>
+      </div>
     </div>
   </div>
 );
